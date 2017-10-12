f0af4eab94bff44ecf125bf4a8ffd843a7b62c9a
Ramon Schmidt <ramonroc@gmail.com>
2017-10-12 20:33:44 -0300

    Add users services, tests and sha1 module

--- src/services/mysql/tests/categories.test.js
+++ src/services/mysql/tests/categories.test.js
@@ -1,6 +1,5 @@
 const test = require('ava')
 const { connection, errorHandler } = require('./setup')
-
 const categories = require('../categories')({ connection, errorHandler })
 
 test.beforeEach(t => connection.query('TRUNCATE TABLE categories'))
@@ -8,6 +7,13 @@ test.beforeEach(t => connection.query('TRUNCATE TABLE categories'))
 test.after.always(t => connection.query('TRUNCATE TABLE categories'))
 
 const create = () => categories.save('category-test')
+
+test('Lista de categorias', async t => {
+  await create()
+  const list = await categories.all()
+  t.is(list.categories.length, 1)
+  t.is(list.categories[0].name, 'category-test')
+})
 
 test('Criação de categoria', async t => {
   const result = await create()
