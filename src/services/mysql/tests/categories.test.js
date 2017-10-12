a29ede52f1696aa651dd8beea736b341a92659a3
Ramon Schmidt <ramonroc@gmail.com>
2017-10-12 19:47:30 -0300

    Category tests and refactoring

---
+++ src/services/mysql/tests/categories.test.js
@@ -0,0 +1,28 @@
+const test = require('ava')
+const { connection, errorHandler } = require('./setup')
+
+const categories = require('../categories')({ connection, errorHandler })
+
+test.beforeEach(t => connection.query('TRUNCATE TABLE categories'))
+// after.always garante mesmo que quando o teste falhe faça o truncate da tabela
+test.after.always(t => connection.query('TRUNCATE TABLE categories'))
+
+const create = () => categories.save('category-test')
+
+test('Criação de categoria', async t => {
+  const result = await create()
+  t.is(result.category.name, 'category-test')
+})
+
+test('Atualização de categoria', async t => {
+  await create()
+  const updated = await categories.update(1, 'category-test-updated')
+  t.is(updated.category.name, 'category-test-updated')
+  t.is(updated.affectedRows, 1)
+})
+
+test('Exclusão de categoria', async t => {
+  await create()
+  const removed = await categories.del(1)
+  t.is(removed.affectedRows, 1)
+})
